315fdef144db7fe4d36e9a384cff0d6c1e6eb8dd
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 16:53:42 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// MILD
+// Create an array called truths and store 3 true facts about yourself
+let truths = ["Mahamadou", "Boy", "Fifteen"]
+
+// console log the 1st element (element is not same as index!)
+console.log(truths[0])
+
+// change the value of the last element
+truths[2] = "NYC"
+
+// console log the last element to check that it's changed
+
+console.log(truths[2])
+// MEDIUM
+// Create a second array calles lies and store 3 fake facts about yourself
+let lies = ["Texas","Senior","Phone"]
+
+// delete a value from the lies array
+lies.pop()
+
+// console log the second list to confirm the value is gone
+
+console.log(lies)
+
+// add a new lie to the lies array
+
+lies.push("Dog")
+console.log(lies)
+// SPICY
+// pick the truths or lies array and console log the 1st element
+
+console.log(lies[0])
+
+// prompt the user to guess if it was true or false
+
+let guess = (prompt
+
+// stretch: how could you check they are right or wrong?
+
+
